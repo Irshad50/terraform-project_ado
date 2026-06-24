fd9461c1441f4bfbd35ffc8b0c50f933c7507afd
Irshad50 <mdirshadalam91190@gmail.com>
2026-06-25 02:09:18 +0530

    Update main.tf

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "vnet" {
 
-  for_each = var.vnets1
+  for_each = var.vnets
 
   name                = each.value.name
   location            = each.value.location
