884d0c7f62a8002cad8b516562e90a0810fd22c4
Irshad50 <mdirshadalam91190@gmail.com>
2026-06-25 02:08:29 +0530

    Update main.tf

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -1,12 +1,12 @@
 resource "azurerm_subnet" "subnet" {
 
-  for_each = var.subnets1
+  for_each = var.subnets
 
   name = each.value.name
 
   resource_group_name = var.resource_groups[each.value.resource_group_key].name
 
-  virtual_network_name = var.vnets1[each.value.virtual_network_key].name
+  virtual_network_name = var.vnets[each.value.virtual_network_key].name
 
   address_prefixes = each.value.address_prefixes
 }
